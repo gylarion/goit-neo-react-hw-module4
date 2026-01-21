301480c59d9128c51a77f005fe9e7f5cbaa474a5
Mykhailo <mmmiiissshhhaaa30@gmail.com>
2026-01-21 20:14:41 +0200

    Initial commit

---
+++ src/services/unsplash-api.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const ACCESS_KEY = import.meta.env.VITE_UNSPLASH_KEY;
+
+axios.defaults.baseURL = "https://api.unsplash.com";
+
+export const fetchImages = async (query, page) => {
+    const response = await axios.get("/search/photos", {
+        params: {
+            query,
+            page,
+            per_page: 12,
+        },
+        headers: {
+            Authorization: `Client-ID ${ACCESS_KEY}`,
+        },
+    });
+
+    return response.data;
+};
